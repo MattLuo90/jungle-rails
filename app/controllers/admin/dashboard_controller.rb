b74fe67475af42c1e39b365c5544a67c467692ad
Mattluo90 <mattluo604@hotmail.com>
2021-06-23 23:32:13 +0000

    update dashboard AR

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -2,6 +2,6 @@ class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: ENV["ADMIN_USERNAME"], password: ENV["ADMIN_PASSWORD"]
   def show
     @product_count = Product.count
-    @categories_count = Product.group(:category_id).count
+    @categories_count = Product.pluck(:category_id).uniq.count
   end
 end
